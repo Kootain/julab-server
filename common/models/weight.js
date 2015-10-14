fa759f3f0bb7fd5453430078c2448a1a3aa6aed4
Tanki <tanki_jong@hotmail.com>
2015-10-14 21:28:28 +0800

    update models

--- common/models/weight.js
+++ common/models/weight.js
@@ -1,10 +1,16 @@
 module.exports = function(Weight) {
-	Weight.remoteMethod(
-        'greet', 
-        {
-          accepts: {arg: 'msg', type: 'string'},
-          returns: {arg: 'greeting', type: 'string'}
-        }
-    );
-
+	// Weight.remoteMethod(
+ //        'greet', 
+ //        {
+ //          accepts: {arg: 'msg', type: 'string'},
+ //          returns: {arg: 'greeting', type: 'string'}
+ //        }
+ //    );
+  Weight.observe('before save',function addTimeInfo(ctx, next){
+    console.log('called');
+    if(ctx.isNewInstance){ //new instance -> create option
+      ctx.instance.gmt_created=(new Date()).toGMTString();
+    }
+    next();
+  });
 };
