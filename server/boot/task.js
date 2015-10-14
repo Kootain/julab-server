151bf6eb3844709761ae9c9bc785882ac98739e9
Tanki <tanki_jong@hotmail.com>
2015-10-14 21:29:25 +0800

    init device finish.

--- server/boot/task.js
+++ server/boot/task.js
@@ -6,77 +6,78 @@ var task= function task(app) {
       };
   var Async = require('async');
   var devices ={};
-
+  var Q = require('q');  
     //TODO get connected devices  devices={MACA:ipA,MACB:ipB.....}
 
   var exec = require('child_process').exec;
   exec('arp -a',function(err,stdout,stderr){
-    var ips=stdout.match(/([0-9a-zA-Z]+:){3}\d+/g);
-    var MACs=stdout.match(/([0-9a-zA-Z]+:){5}[0-9a-zA-Z]+/g);
+    var ips=stdout.match(/(\d+\.){3}\d+/g);
+    var MACs=stdout.match(/([0-9a-zA-Z]{2}:){5}[0-9a-zA-Z]+/g);
 
     for (var i = ips.length - 1; i >= 0; i--) {
       devices[MACs[i]]=ips[i];
     };
-    var knownDevices = [], unKownDevices=[];
+    var onlineDevices = [],    // [{MAC:??, ip:??, name:??, type:??}, ... ]
+        offlineDevices=[],     // [{MAC:??, name:??}, ... ]
+        unKownDevices=[];      // [{MAC：??, ip:??}, ... ]
     var deviceType=['Scale'];
+
+    function getDeviceListOfAType(type){  
+      var deferred = Q.defer();//
+      app.models[type]
+              .find(
+                {where:{MAC:{inq:MACs}}},
+                function(data){
+                  for (var i = data.length - 1; i >= 0; i--) {
+                    if(devices[data[i].MAC]){   //if exist in 'devices' => online  else  offline
+                      devices[data[i].MAC]['known']=true;
+                      data[i]['type']=type;
+                      onlineDevices.push(data[i]);
+                    } else {
+                      offlineDevices.push(data[i]);
+                    }
+                  };
+                  deferred.resolve();
+                });
+      return deferred.promise;
+    }
+
+    var jobs=[];
     for (var i = deviceType.length - 1; i >= 0; i--) {
-      app.models[deviceType[i]]
-              .find({where:{MAC:{inq:MACs}}});
-      knownDevices = knownDevices.concat(devs);
-          //.map( function(e) { return {'MAC':e.MAC,'name':name,ip:'',type:deviceType[i]} }));
+      jobs.push(getDeviceListOfAType(deviceType[i]));
     };
 
-    for (var i = knownDevices.length - 1; i >= 0; i--) {
-      if( devices[knownDevices[i]] ) {
-        knownDevices[i].ip = device[knownDevices[i]];
-        delete devices[knownDevices[i]];
+    Q.all(jobs)  
+    .done(function(){   
+      //mark unknown 
+      for(var mac in devices){
+        if(!devices[mac]['known']) unKownDevices.push(mac);
+      } 
+
+      //generate devices socket, bind on app
+      setTimeout(function(){
+          app.onlineDevices = {
+            list:onlineDevices,
+            connectors:onlineDevices.map(function(e){ return new Device(e.name, e.ip, SERVER_PORT)})
+          };
+      },0);
+
+      setTimeout(function(){
+          app.unKownDevices ={
+            list:unKownDevices,
+            connctors:unKownDevices.map(function(e){ return new Device('unknown', e.ip, SERVER_PORT)})
+          };
+      },0);
+      
+      
+
+      app.offlineDevices = offlineDevices;
+
+      // register task for online devices.
+      for (var i = knownDevices.length - 1; i >= 0; i--) {
+        tasks[app.onlineDevices.list[i]['type']](app.onlineDevices.connectors[i]);
       }
-    }
-
-    for (var i in devices) {
-      unKownDevices.push({'MAC':i, 'ip':devices[i]});
-    };
-
-    knownDevices = knownDevices
-                    .filter(function(e){ return e.ip})
-                    .map(function(e){ return new Device(e.name, e.ip, SERVER_PORT)});
-    unKownDevices = unKownDevices.map(function(e){ return new Device('unknown', e.ip, SERVER_PORT)})
-
-    app.unKownDevices = unKownDevices;
-    // Async.series([
-    //   function(){  //check unknown devices
-    //     for (var i = unKownDevices.length - 1; i >= 0; i--) {
-    //       unKownDevices[i].connect(function(d){
-    //         if(!d) {
-    //           colorlog.log('unKown');
-    //         } else {
-    //           var certainDevice
-    //           app.models[deviceType[i]].create
-    //           //TODO 1.add to db 2.add to knownDevices 
-    //         }
-    //         unKownDevices[i].close(); 
-    //       });
-    //     };
-    //   },
-    //   function(){
-    //     for (var i = unKownDevices.length - 1; i >= 0; i--) {
-    //         unKownDevices[i].close(); 
-    //     };
-    //   },
-    //   function(){  //build socket connection
-    //     for (var i = knownDevices.length - 1; i >= 0; i--) {
-    //       Scale(knownDevices[i]);
-    //       //knownDevices[i].connect();
-    //       //TODO  use different callback to process different action of devices.
-    //     };
-    //   }
-    // ]);
-     //build socket connection
-    for (var i = knownDevices.length - 1; i >= 0; i--) {
-      tasks[knownDevices[i].type](knownDevices[i]);
-      //knownDevices[i].connect();
-      //TODO  use different callback to process different action of devices.
-    }
+    });
   });
 }
 
